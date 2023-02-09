79c4e9548c1a38528f133028946e2226df0845a7
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-02-09 17:16:16 +0100

    feat added es campo minato

---
+++ js/app.js
@@ -0,0 +1,45 @@
+// CONTROLLIAMO CHE IL COLLEMENTO FUNZIONI DANDO IL BENVENUTO ALL'UTENTE
+// alert('BENVENUTO A CAMPO MINATO')
+// COLLEGARE IL BOTTONE PLAY AD UN EVENT LISTENER PER FAR APPARIRE LA GRIGLIA DI GIOCO
+
+const btn = document.getElementById("mybuttonplay")
+// QUANDO L'UTENTE FARE UN CLICK SI CE EVENT LISTENER CON UN FUNZIONE 
+btn.addEventListener("click", function () {
+    // CALCOLO LE CELLE DELLE GRIGLIA 
+    let latoGriglia = 10
+    let numeroCelle = latoGriglia * latoGriglia
+    console.log(latoGriglia, numeroCelle)
+    console.log("IL BOTTONE FUNZIONA");
+    // COLLEMENTO AL DOM
+    const grigliaElement = document.querySelector('.griglia-caselle-di-gioco')
+    console.log(grigliaElement)
+    // CREO LE DIV CON UN CICLO FOR E USO APPEND PER METTERLI ALL'INZIO DELLA GRIGLIA
+    for (let i = 0; i < numeroCelle; i++) {
+        let num = i + 1
+	    console.log(num)
+        let cellaElement = document.createElement('div')
+        console.log(cellaElement)
+        grigliaElement.append(cellaElement)
+        cellaElement.classList.add("cella", "width");
+        cellaElement.append(num)
+        
+    }
+    // RIPRENDO IL TAG CELLA CREATO IN PRECEDENZA 
+    const celleTotali = document.querySelectorAll('.cella')
+    // CREO UN CICLO FOR CHE AL CLICK MI DICE IL NUMERO DELLA CASELLA E MI IMPOSTA IL COLORE 
+    // DI BACKGROUND
+
+    for (let i = 0; i < celleTotali.length; i++) {
+        const cella = celleTotali[i]
+        
+        cella.addEventListener('click', function () {
+            console.log(i + 1)
+            cella.style.backgroundColor = "blue";
+            
+
+        })
+
+    }
+  
+
+});
